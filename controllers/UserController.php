7ea17588d0c5c2df7d1282dd6f7ac664915c611a
nandhini <nandhiniiabacies.com>
2016-04-29 19:58:04 +0530

    profile page update

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -218,24 +218,35 @@ class UserController extends Controller
             throw new NotFoundHttpException('The requested page does not exist.');
         }
     }
-	public function actionProfile($id){
+	/**
+     * Save the UserProfile model based on its primary key value.
+     * If the model is saved, view action will be called.
+     * If the model is not saved, will be in the same profile actionss.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionProfile($id){
             
-		$model = User::find()->where(['id' => $id])->one();		// \Yii::$app->user->id] 
-		$profile = Profile::find()->where(['user_id' => $id])->one();	// \Yii::$app->user->id]
+        $model = User::find()->where(['id' => $id])->one();		// \Yii::$app->user->id] 
+        $profile = Profile::find()->where(['user_id' => $id])->one();	// \Yii::$app->user->id]
 		
        // if ($model->load(Yii::$app->request->post()) && $profile->load(Yii::$app->request->post())) {		
                 if(Yii::$app->request->post()){		
-                    $profile->profile_photo = UploadedFile::getInstance($profile, 'profile_photo');
-                    $profile->profile_bg_image = UploadedFile::getInstance($profile, 'profile_bg_image');		
-                    $prof_img_path =  $profile->profile_photo->baseName . '.' . $profile->profile_photo->extension;
-                    $background_img_path =  $profile->profile_bg_image->baseName . '.' .$profile->profile_bg_image->extension;
+             
+                    $profile->firstname = Yii::$app->request->post('firstname');
+                    $profile->lastname = Yii::$app->request->post('lastname');
+                    $profile->short_description = Yii::$app->request->post('short_description');
+                   // $profile->profile_photo = UploadedFile::getInstance($profile, 'profile_photo');
+                   /// $profile->profile_bg_image = UploadedFile::getInstance($profile, 'profile_bg_image');		
+                   // $prof_img_path =  $profile->profile_photo->baseName . '.' . $profile->profile_photo->extension;
+                  //  $background_img_path =  $profile->profile_bg_image->baseName . '.' .$profile->profile_bg_image->extension;
                     $idToUpdate =  \Yii::$app->user->id;				
             //if ($model->validate()) {       
-                    $profile->profile_photo->saveAs('uploads/'.$profile->profile_photo->baseName . '.' . $profile->profile_photo->extension);
-                    $profile->profile_bg_image->saveAs('uploads/'.$profile->profile_bg_image->baseName . '.' . $profile->profile_bg_image->extension);
+                   // $profile->profile_photo->saveAs('uploads/'.$profile->profile_photo->baseName . '.' . $profile->profile_photo->extension);
+                   // $profile->profile_bg_image->saveAs('uploads/'.$profile->profile_bg_image->baseName . '.' . $profile->profile_bg_image->extension);
                     //$base_url = Yii::$app->baseUrl();
-                    $profile->profile_photo  = '/uploads/'.$prof_img_path;
-                    $profile->profile_bg_image = '/uploads/'.$background_img_path;
+                  //  $profile->profile_photo  = '/uploads/'.$prof_img_path;
+                   // $profile->profile_bg_image = '/uploads/'.$background_img_path;
                     $model->save();
                     $profile->save();
             //}
@@ -249,5 +260,34 @@ class UserController extends Controller
 			]);
 		}
 		
+	}	
+    /**
+     * Uploads the Profile Picture
+     * @return uploaded file name
+     */    
+        public function actionPhoto(){
+            $session = Yii::$app->session;
+            $language = $session->get('userid');
+            $idToUpdate =  \Yii::$app->user->id;
+            $profile = Profile::find()->where(['user_id' => $idToUpdate])->one();
+                if (Yii::$app->request->isAjax) {                
+                   $move = Yii::$app->basePath.'\web\uploads\\';              
+                   $moveto = $move.$_FILES["file"]['name'];
+                   $_FILES["file"]['tmp_name'];
+                   $_FILES["file"]['size'];
+                   $_FILES['file']['error'];
+                        if(file_exists($move.$_FILES["file"]['name'])) {
+                             chmod($move.$_FILES["file"]['name'],0755); //Change the file permissions if allowed
+                             unlink($move.$_FILES["file"]['name']); //remove the file
+                         }
+                   move_uploaded_file($_FILES['file']['tmp_name'], $move.$_FILES["file"]['name']);
+                   $prof_img_path =  $_FILES["file"]['name'];
+                   $profile->profile_photo = "uploads/".$prof_img_path;    
+                   $profile->save();
+                   \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+                             return [
+                                 'code' => $_FILES["file"]['name'],
+                             ];
+               }  
 	}	
 }
