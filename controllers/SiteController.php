bd33fce04c4522e2cf09b56e83133285569230eb
nandhini <nandhiniiabacies.com>
2016-05-25 16:01:04 +0530

    is Mobile!

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -130,6 +130,23 @@ class SiteController extends Controller
     } 
     
     public function isMobile(){
-         return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+         $aMobileUA = array(
+        '/iphone/i' => 'iPhone', 
+        '/ipod/i' => 'iPod', 
+        '/ipad/i' => 'iPad', 
+        '/android/i' => 'Android', 
+        '/blackberry/i' => 'BlackBerry', 
+        '/webos/i' => 'Mobile'
+    );
+
+    //Return true if Mobile User Agent is detected
+    foreach($aMobileUA as $sMobileKey => $sMobileOS){
+        if(preg_match($sMobileKey, $_SERVER['HTTP_USER_AGENT'])){
+            return true;
+        }
+    }
+    //Otherwise return false..  
+    return false;
+       //  return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
     }
 }
