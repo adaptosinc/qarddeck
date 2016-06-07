0904e5c143fbccac2d6e1734ae55476caba4c0f0
dencygb <dency@abacies.com>
2016-06-07 13:23:14 +0530

    Qard stream initialised

---
+++ views/qard/qard_stream.php
@@ -0,0 +1,126 @@
+<?php 
+
+
+?>
+<section class="main-profile">
+	<!--<ul class="profile-title desktop-view">
+		<li><h4>Latest from QardDeck</h4></li>
+	</ul>-->
+	<ul class="profile-title mobile-page">
+		<li><h4>Latest from QardDeck</h4></li>
+		<li class="pull-right">
+			<ul>
+				<li><button class="btn btn-default signin">Qards</button></li>
+				<li><button class="btn btn-default">Decks</button></li>
+			</ul>
+		</li>
+	</ul>                    
+	
+	<div class="main-content">
+		<div class="popular-qards">     <!-- popular qard list -->
+			<div class="row">
+				<div class="col-sm-9 col-md-9">
+					<div class="row">
+						<div class="col-sm-3 col-md-3 col-md-offset-1">     <!-- qard -->
+							<div class="qard-content">
+								<div id="add-block" class="qard-div add-block">
+										<input id="qard_id" type="hidden" value="23"><div id="blk_2" class="bgimg-block parent_current_blk" style="height: 150px !important; opacity: 1; background-image: url(&quot;/works/qarddeck/web/uploads/block/rand_8748time_1465279541qid_23.JPG&quot;); background-size: cover;"><div class="bgoverlay-block" style="opacity: 0.7; background-color: rgb(250, 239, 239);"><div data-height="4" style="height:150px;" data-block_id="25" data-theme_id="38" class="text-block current_blk"><div style="text-align: center;"><b style="color: rgb(0, 0, 0); line-height: 1.42857;">&nbsp;</b></div><div style="text-align: center;"><b style="line-height: 1.42857;"><font color="#1c09f0"><br></font></b></div><div style="text-align: center;"><b style="line-height: 1.42857;"><i><u><font color="#1c09f0" size="3">vhvhjvjhjgcccc hgvjhsdgfv vjsfc s jkhksdfh jkhjksd jkhksdjhkf hhf fo hiof fi eoir rioeo cvxbsdvnc vferhfgh vd</font></u></i></b><br></div></div></div></div><div id="blk_3" class="bgimg-block parent_current_blk" style="height: 187.5px; opacity: 1;"><div class="bgoverlay-block" style="opacity: 0.7; height: 187.5px; background-color: rgb(240, 131, 131);"><div class="text-block current_blk" data-height="5" style="height: 187.5px;"><span class="url-qard-block" id="url_parentblk_3"><span class="col-sm-12 col-md-12" id="title_desc_urlblk_3"><span class="url-content"><h4><font color="#2c1bf5">jquery - Gravity Forms Engineering - Stack Overflow</font></h4><span class="url-text"><p>Am tearing my hair out on a Gravity Form question. Anyone here know the GF hook system?  Here is the deal.  I have a 3 part form.  The 1st part contains several radio button style questions.  What I want to do is this: dvd</p></span></span></span></span></div></div></div>
+										
+										
+															<div id="working_div" class="working_div active"><div id="blk_1" class="bgimg-block parent_current_blk" style="height: 262.5px;">
+																<div class="bgoverlay-block" style="height: 262.5px;">
+																	<div class="text-block current_blk working_div" data-height="7" style="height: 262.5px;" unselectable="off" contenteditable="true"><span class="review-qard" id="url_parentblk_1"><span class="col-sm-12 col-md-12" id="title_desc_urlblk_1"><span class="url-content"><h4>javascript - DataTable width is going beyond a panel - Stack Overflow</h4><span class="url-text"><p>Good day everybody.Im new on ASP.Net MVC 4, Im using DataTable plugin for my table and Im encountering the following problems. 1. DataTable Width is going beyond the width of the panel that contained it. 2. It didn't generate a child row that will supposedly show the columns that will not fit in the panel's width.<span style="line-height: 18.5714px;">DataTable Width is going beyond the width of the panel that contained it. 2. It didn't generate a child row that will supposedly show the columns that will not fit in the panel's width dfvedf</span></p></span></span></span></span></div>                                    
+																</div>                                
+															</div></div>
+											
+										<h4 class="add-another" onclick="add_block(event)" style="display: none;">Add another block <span><img src="/works/qarddeck/web/images/add.png" alt="add"></span></h4>
+								</div>								
+							</div>
+							<div class="qard-bottom">
+								<ul class="qard-tags">
+									<li class="pull-left">#tag#tag#tag</li>
+									<li class="pull-right">x days ago</li>
+								</ul>
+								<h4>Author Full name</h4>
+								<ul class="social-list">
+									<li><a href=""><img src="images/heart.png" alt=""><br />500</a></li>
+									<li><a href=""><img src="images/comment-dark.png" alt=""><br />500</a></li>
+									<li><a href=""><img src="images/certify.png" alt=""><br />500</a></li>
+									<li><a href=""><img src="images/share.png" alt=""><br />500</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-sm-3 col-md-3 col-md-offset-1">     <!-- qard -->
+							<div class="qard-content">
+								
+							</div>
+							<div class="qard-bottom">
+								<ul class="qard-tags">
+									<li class="pull-left">#tag#tag#tag</li>
+									<li class="pull-right">x days ago</li>
+								</ul>
+								<h4>Author Full name</h4>
+								<ul class="social-list">
+									<li><a href=""><img src="images/heart.png" alt=""><br />500</a></li>
+									<li><a href=""><img src="images/comment-dark.png" alt=""><br />500</a></li>
+									<li><a href=""><img src="images/certify.png" alt=""><br />500</a></li>
+									<li><a href=""><img src="images/share.png" alt=""><br />500</a></li>
+								</ul>
+							</div>
+								<div class="comments">
+									<h5>Latest Comment</h5>
+									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor</p>
+									<ul class="comment-tags">
+										<li class="pull-left">Commentor Name</li>
+										<li class="pull-right">x mins ago</li>
+									</ul>
+								</div>                                            
+						</div>
+						<div class="col-sm-3 col-md-3 col-md-offset-1">     <!-- qard -->
+							<div class="qard-content">
+								
+							</div>
+							<div class="qard-bottom">
+								<ul class="qard-tags">
+									<li class="pull-left">#tag#tag#tag</li>
+									<li class="pull-right">x days ago</li>
+								</ul>
+								<h4>Author Full name</h4>
+								<ul class="social-list">
+									<li><a href=""><img src="images/heart.png" alt=""><br />500</a></li>
+									<li><a href=""><img src="images/comment-dark.png" alt=""><br />500</a></li>
+									<li><a href=""><img src="images/certify.png" alt=""><br />500</a></li>
+									<li><a href=""><img src="images/share.png" alt=""><br />500</a></li>
+								</ul>
+							</div>                                            
+						</div>                                        
+					</div>  <!-- row -->
+				</div>
+				<div class="col-sm-3 col-md-3">
+					<div class="featured-tags">     <!--featured tags -->
+						<h4>Featured Tags</h4>
+						<button class="btn btn-default">Tag</button>
+					</div>
+						<div class="sidebar-qard">     <!-- side qard -->
+							<div class="qard-content">
+								
+							</div>
+							<div class="qard-bottom">
+								<ul class="qard-tags">
+									<li class="pull-left">#tag#tag#tag</li>
+									<li class="pull-right">x days ago</li>
+								</ul>
+								<h4>Author Full name</h4>
+								<ul class="social-list">
+									<li><a href="">20<img src="images/newqard.png" height="50px" alt=""></a></li>
+									<li><a href=""><img src="images/heart.png" alt=""><br />500</a></li>                                                    
+									<li><a href=""><img src="images/certify.png" alt=""><br />500</a></li>
+									<li><a href=""><img src="images/share.png" alt=""><br />500</a></li>
+								</ul>
+							</div>                                           
+						</div>                                    
+				</div>
+			</div>          <!-- row -->
+		</div>
+	</div>
+</section>
