d6a6145c5dc61e7907171d761ced327288aa6e41
dencygb <dency@abacies.com>
2016-07-27 13:33:43 +0530

    sorting blocks jerks fixed

--- views/qard/edit.php
+++ views/qard/edit.php
@@ -976,6 +976,7 @@ $this->title = 'Edit Qard';
 				ui.item.trigger("dblclick");
 				totalBlocks = $("#add-block").find(".current_blk").length;
 				var max_allowed_position = parseInt(totalBlocks+2); 
+				var total = totalHeight();	
 				if( total <= 16 && $('.add-another').index() !== max_allowed_position)
 					return false;
 				
