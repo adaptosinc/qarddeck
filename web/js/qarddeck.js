0fe7ebaadae33382108691297f2d6e6d84b51dc1
dencygb <dency@abacies.com>
2016-10-17 14:47:10 +0530

    add link sync true

--- web/js/qarddeck.js
+++ web/js/qarddeck.js
@@ -633,7 +633,7 @@
 			//	url : "<?=Url::to(['block/add-text'], true)?>",
 				url : plugin.settings.addUrlDataUrl,
 				type: "POST",
-				async : true,
+				//async : true,
 				data: { 'url_title':$("input[name=url-title]").val(),
 						'block_id':$("#working_div .current_blk").attr('data-block_id'),
 						'url_description' : $("input[name=url-desc]").val(),
